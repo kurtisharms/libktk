0f2182c920e095f401152c3ad5afe474a8b2749a
Kurtis Harms <kurtisharms@users.noreply.github.com>
2008-06-28 10:31:32 +0000

    FrameBase updates

--- libKTK/FrameBase.cpp
+++ libKTK/FrameBase.cpp
@@ -12,5 +12,22 @@ namespace Ktk
     {
         //dtor
     }
+    void FrameBase::add(Widget* w)
+        {
+            WidgetList.push_back(w);
+        }
+
+        void FrameBase::remove(Widget* w)
+        {
+            Widget* iw;
+            for (int i=0; i<WidgetList.size(); i++) {
+            //for (vector<Widget*>::iterator it = WidgetList.begin(); it!=WidgetList.end(); ++it) {
+                iw = WidgetList[i];
+                if(iw->getId() == w->getId())
+                {
+                    WidgetList.erase(WidgetList.begin() + i);
+                }
+            }
+         }
 
 } // Ktk namespace
--- libKTK/FrameBase.h
+++ libKTK/FrameBase.h
@@ -12,33 +12,11 @@ namespace Ktk
         FrameBase();
         virtual ~FrameBase();
         vector<Ktk::Widget*> WidgetList;
+        void add(Widget* w);
+        void remove(Widget* w);
     protected:
     private:
-    public:
-        template <class WidgetType>
-        void add(WidgetType *w)
-        {
-            Widget *x = (Widget*) w;
-            WidgetList.push_back(x);
-            /*for (int i=0; i<WidgetList.size(); i++) {
-                Widget* p = WidgetList[i];
-                p->draw();
-            }*/
-        }
 
-        template <class WidgetType>
-        void remove(WidgetType *w)
-        {
-            Widget* iw;
-            for (int i=0; i<WidgetList.size(); i++) {
-            //for (vector<Widget*>::iterator it = WidgetList.begin(); it!=WidgetList.end(); ++it) {
-                iw = WidgetList[i];
-                if(iw->getId() == w->getId())
-                {
-                    WidgetList.erase(WidgetList.begin() + i);
-                }
-            }
-         }
     };
 
 } // Ktk namespace
