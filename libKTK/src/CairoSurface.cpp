ef1aee26322f418b24f35d62268257bc86e044d8
Kurtis Harms <kurtisharms@users.noreply.github.com>
2008-07-17 11:30:30 +0000

    Added CairoSurface base class.

---
+++ libKTK/src/CairoSurface.cpp
@@ -0,0 +1,16 @@
+#include "CairoSurface.h"
+
+namespace Ktk
+{
+
+    CairoSurface::CairoSurface()
+    {
+        //ctor
+    }
+
+    CairoSurface::~CairoSurface()
+    {
+        //dtor
+    }
+
+} // Ktk namespace
---
+++ libKTK/src/CairoSurface.h
@@ -0,0 +1,18 @@
+#ifndef CAIROSURFACE_H
+#define CAIROSURFACE_H
+
+namespace Ktk
+{
+
+    class CairoSurface
+    {
+    public:
+        CairoSurface();
+        virtual ~CairoSurface();
+    protected:
+    private:
+    };
+
+} // Ktk namespace
+
+#endif // CAIROSURFACE_H
